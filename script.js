8fea232ee13807e0b16abcecc05e61081b6ad082
claudiap25 <claudiapad2004@gmail.com>
2025-08-14 00:13:10 +0300

    bravo

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById("clickButton").addEventListener("click", function() {
     document.getElementById("message").textContent = "You clicked the button!";
 });
+/*sa moara camiluta mea ca nu mai pot vereeeeeeeeeeeeeeeeeeee
+barcelona
+alea
+alea*/
+/*sa moara camiluta mea ca nu mai pot vereeeeeeeeeeeeeeeeeeee
+barcelona
+alea
+alea*/
+/*sa moara camiluta mea ca nu mai pot vereeeeeeeeeeeeeeeeeeee
+barcelona
+alea
+alea*/
